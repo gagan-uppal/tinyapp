f0b054c2aaaa18abc494b693d349e05b2c0ed95d
Gagandeep Uppal <guppal.profile@gmail.com>
2021-09-27 23:43:34 +0000

    post request added

--- express_server.js
+++ express_server.js
@@ -1,23 +1,51 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
+/* Generates a random string, used for creating short URLs and userIDs */
+function generateRandomString() {
+  let randomString = "";
+  const length = 6;
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++)
+  randomString += chars.charAt(Math.floor(Math.random() * chars.length));
+  return randomString;
+}
+
+/* const urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca"},
   "9sm5xK": {longURL: "http://www.google.com"}
+}; */
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   console.log(urlDatabase[req.params.shortURL].longURL);
   console.log(req.params.shortURL === "b2xVn2")
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {

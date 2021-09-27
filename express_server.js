5c71a3450850ecd754c1c42a3e95025e2fa4df1e
Gagandeep Uppal <guppal.profile@gmail.com>
2021-09-27 19:01:39 +0000

    added new routes /url /hello

--- express_server.js
+++ express_server.js
@@ -12,6 +12,17 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
